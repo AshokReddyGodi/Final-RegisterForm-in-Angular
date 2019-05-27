46471f0832cdaa089972b204241a5dd4ee6c5c03
AshokReddyGodi <ashokgodi1996@gmail.com>
2019-05-27 15:09:50 +0530

    Created Register Form in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { FormvalidationComponent } from './formvalidation/formvalidation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormvalidationComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
